2672332470e544a58ee478e15c920b014c1219a4
Alexander-N <A.Niederbuehl@gmail.com>
2015-09-17 23:12:42 +0200

    Refactoring

--- plotWeatherStations.js
+++ plotWeatherStations.js
@@ -9,45 +9,14 @@ function setYear(year) {
         .property("value", year);
 }
 
-function nestCount(key, weatherStations){
-    var nest = d3.nest()
-        .key(function(d) {
-            return d[key];
-        });
-
-    var stationsByYear = nest
-        .map(weatherStations);
-
-    var nStations = nest
-        .rollup(function(d){
-            return d.length;
-        })
-        .map(weatherStations);
-    return [stationsByYear, nStations];
-}
-
-function plotLine(nStarted, nEnded, width, height) {
-    var sum = 0;
-    var sums = [];
-    var years = [];
-
-    for(var year=1900; year<2016; year++) {
-        sum += nStarted[year] || 0;
-        sum -= nEnded[year] || 0;
-        sums.push(sum);
-        years.push(year);
-    }
-
-    var yearsAndSums = d3.zip(years, sums);
-
-    // add the line chart
+function LinePlot(width, height, nStations) {
     var x = d3.scale.linear()
         .range([70, width-10])
         .domain([1900, 2015]);
 
     var y = d3.scale.linear()
         .range([height-25, 10])
-        .domain(d3.extent(sums));
+        .domain(d3.extent(d3.values(nStations)));
 
     var xAxis = d3.svg.axis()
         .scale(x)
@@ -62,17 +31,17 @@ function plotLine(nStarted, nEnded, width, height) {
         .x(function(d) { return x(d[0]); })
         .y(function(d) { return y(d[1]); });
 
-    var svgLine = d3.select("div#chart")
+    var svg = d3.select("div#chart")
         .append("svg")
         .attr("width", width)
         .attr("height", height);
 
-    svgLine.append("g")
+    svg.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + (height - 20) + ")")
         .call(xAxis);
 
-    svgLine.append("g")
+    svg.append("g")
         .attr("class", "y axis")
         .attr("transform", "translate(70,0)")
         .call(yAxis)
@@ -83,27 +52,228 @@ function plotLine(nStarted, nEnded, width, height) {
         .attr("transform", "rotate(-90)")
         .text("Number of Weather Stations");
 
-    svgLine.append("path")
+    var years = Object.keys(nStations).sort();
+    var yearsAndSums = years.map(function(year){ return [year, nStations[year]]; });
+    svg.append("path")
         .datum(yearsAndSums)
         .attr("class", "line")
         .attr("d", line);
 
-    svgLine.append("circle")
+    var circle = svg.append("circle")
         .attr("r", 10)
         .attr("cx", x(1900))
         .attr("cy", y(0))
         .attr("id", "line");
 
-    svgLine.x = x;
-    svgLine.y = y;
-    svgLine.nStations = sums;
-    return svgLine;
+    this.svg = svg;
+
+    this.update = function(year) {
+        circle
+        .attr("cx", x(year))
+        .attr("cy", y(nStations[year]));
+    };
+
+    this.mouseToYear = function(mouseCoordinate) {
+        var year = Math.round(x.invert(mouseCoordinate));
+        return Math.min(2015, Math.max(1900, year));
+    };
+
 }
 
-function updateLine(svgLine, year) {
-    svgLine.select("circle")
-        .attr("cx", svgLine.x(year))
-        .attr("cy", svgLine.y(svgLine.nStations[year-1900]));
+function StationCircles(gCircles, circleRadius) {
+
+    var tooltip = d3.select("div#map")
+        .append("div")
+        .attr("class", "tooltip hidden");
+
+    function mouseIn(circle, stationName) {
+         //offsets for tooltips
+        var divMap = document.getElementById("map");
+        var offsetL = divMap.offsetLeft+15;
+        var offsetT = divMap.offsetTop+10;
+        var mouse = d3.mouse(d3.select("#svg-map").node());
+        var left = mouse[0]+offsetL;
+        var top = mouse[1]+offsetT;
+
+        tooltip
+            .classed("hidden", false)
+            .attr(
+                "style",
+                "left:" + left + "px;top:" + top + "px"
+            )
+            .html(stationName);
+        d3.select(circle).attr("r", circleRadius*4);
+    }
+
+    function mouseOut(circle) {
+        tooltip
+            .classed("hidden", true);
+        d3.select(circle).attr("r", circleRadius);
+
+    }
+
+    function getId(d) { return d.id; }
+    function getLong(d) { return d.coordinates[0]; }
+    function getLat(d) { return d.coordinates[1]; }
+
+    this.update = function(year) {
+        var currentlyActiveStations = this.activeStations[year];
+        var circles = gCircles.selectAll("circle")
+            .data(currentlyActiveStations, getId);
+
+        circles
+            .exit()
+            .remove();
+
+        var newCircles = circles
+            .enter()
+            .append("circle");
+
+        newCircles
+            .attr("cx", getLong)
+            .attr("cy", getLat)
+            .attr("r", circleRadius);
+
+        newCircles
+            .on("mousemove", function(d, i) {
+                mouseIn(this, d.name);
+            })
+            .on("mouseout", function(d, i){
+                mouseOut(this);
+            });
+    };
+
+    this.zoom = function() {
+        circleRadius = 1.15 / ((0.5*d3.event.scale) + 0.5);
+        gCircles.selectAll("circle")
+            .attr("r", circleRadius);
+    };
+}
+
+function Interaction(stationCircles, linePlot){
+        linePlot.svg.classed("hidden", true);
+        var year = 1900;
+        d3.select("#year-slider").on("mousemove", function(e){
+            year = this.value;
+            update(year);
+        });
+        var runAnimation = false;
+        var animationButton = d3.select("button.animation");
+        function toggleAnimation() {
+            if (runAnimation === true) {
+                runAnimation = false;
+                animationButton.text("Start");
+            }
+            else if (runAnimation === false) {
+                runAnimation = true;
+                animationButton.text("Stop");
+                animation();
+            }
+        }
+        function toggleGraph() {
+            if (linePlot.svg.classed("hidden")) {
+                linePlot.svg.classed("hidden", false);
+            }
+            else {
+                linePlot.svg.classed("hidden", true);
+            }
+        }
+        d3.select("button.graph")
+            .on("click", toggleGraph);
+
+        //Add click and drag behaviour for red circle in graph
+        function linePlotUpdate(mouseCoordinate) {
+            year = linePlot.mouseToYear(mouseCoordinate);
+            update(year);
+        }
+        var drag = d3.behavior.drag()
+            .on("drag", function(d,i) {
+                linePlotUpdate(d3.event.x);
+            });
+        linePlot.svg.select("circle").call(drag);
+
+        linePlot.svg
+            .on("click", function(d,i) {
+                linePlotUpdate(d3.mouse(linePlot.svg.node())[0]);
+            });
+
+        animationButton
+            .on("click", toggleAnimation);
+
+        function update(year) {
+            setYear(year);
+            linePlot.update(year);
+            stationCircles.update(year);
+        }
+
+        function animation() {
+            if (runAnimation === true) {
+                if (year <= 2015) {
+                    update(year);
+                    year++;
+                    setTimeout(animation, 300);
+                } else {
+                    runAnimation = false;
+                    animationButton.text("Start");
+                }
+            }
+        }
+
+}
+
+function Countries(width, height, scale, geoData) {
+    var svg = d3.select("div#map")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("id", "svg-map")
+    var g = svg
+        .append("g")
+    var projection = d3.geo.mercator()
+        .scale(scale*4.4)
+        .translate([width / 2.6, height*1.58]);
+    var path = d3.geo.path().projection(projection);
+
+    g.selectAll("path")
+        .data(geoData.features)
+        .enter()
+        .append("path")
+        .attr("class", "countries")
+        .attr('d', path);
+    g.style("stroke-width", "0.9");
+
+    this.projection = projection;
+    this.g = g;
+    this.svg = svg;
+
+    this.zoom = function() {
+        g.style("stroke-width", (0.9 / ((0.5*d3.event.scale) + 0.5)));
+        g.attr(
+            "transform",
+            "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")"
+        );
+    };
+}
+
+function nestByProperty(property, weatherStations) {
+    var nest = d3.nest()
+        .key(function(d) {
+            return d[property];
+        });
+    var stationsByProperty = nest
+        .map(weatherStations);
+    return stationsByProperty;
+}
+
+function getNumberOfStations(stationsStarted, stationsEnded) {
+    var sum = 0;
+    var nStations = {};
+    for(var year=1900; year<2016; year++) {
+        sum += (stationsStarted[year] || []).length;
+        sum -= (stationsEnded[year] || []).length;
+        nStations[year]= sum;
+    }
+    return nStations;
 }
 
 function getActiveStations(stationsStart, stationsEnd) {
@@ -131,61 +301,25 @@ function draw(geoData) {
     var scale = 115;
     var width = 750 * scale / 120,
         height = 600 * scale / 120;
-
-    var svg = d3.select("div#map")
-        .append("svg")
-        .attr("width", width)
-        .attr("height", height);
-
-    var tooltip = d3.select("div#map")
-        .append("div")
-        .attr("class", "tooltip hidden");
-
-    var gMap  = svg
-        .append("g")
-        .attr("class", "map");
-
-    var circleRadius = scale/100;
-
-
-    var projection = d3.geo.mercator()
-        .scale(scale*4.4)
-        .translate([width / 2.6, height*1.58]);
-
-    var path = d3.geo.path().projection(projection);
-
-    var map = gMap.selectAll("path")
-        .data(geoData.features)
-        .enter()
-        .append("path")
-        .attr("class", "countries")
-        .attr('d', path);
-
-    gMap.style("stroke-width", "0.9px");
-    var gCircles = gMap.append("g");
+    var map = new Countries(width, height, scale, geoData);
+    var gCircles = map.g.append("g");
+    var stationCircles = new StationCircles(gCircles, scale/100);
 
     function zoom() {
-        circleRadius = scale/100 / ((0.5*d3.event.scale) + 0.5);
-        gCircles.selectAll("circle")
-            .attr("r", circleRadius + "px");
-        gMap.style("stroke-width", (0.9 / ((0.5*d3.event.scale) + 0.5)) + "px");
-        gMap.attr(
-            "transform",
-            "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")"
-        );
+        stationCircles.zoom();
+        map.zoom();
     }
-
     var zoomListener = d3.behavior.zoom()
         .translate([0, 0])
         .scale(1)
         .on("zoom", zoom);
+    zoomListener(map.svg);
 
-    zoomListener(svg);
     d3.csv("europe.csv", function(d) {
         return {
             id : d.USAF,
             name : d["STATION NAME"],
-            coordinates : projection([+d.LON, +d.LAT]),
+            coordinates : map.projection([+d.LON, +d.LAT]),
             start_year : +d.BEGIN.slice(0,4),
             end_year : +d.END.slice(0,4)
         };
@@ -193,147 +327,13 @@ function draw(geoData) {
     processData);
 
     function processData(weatherStations){
-        var stations = nestCount("start_year", weatherStations);
-        var stationsStart = stations[0],
-        nStarted = stations[1];
-
-        stations = nestCount("end_year", weatherStations);
-        var stationsEnd = stations[0],
-        nEnded = stations[1];
-        stationsEnd[2015] = [];
-        nEnded[2015] = 0;
-
-        var svgLine = plotLine(nStarted, nEnded, width, height);
-        svgLine.classed("hidden", true);
-        var activeStations = getActiveStations(stationsStart, stationsEnd);
-        var year = 1900;
-
-        d3.select("#year-slider").on("mousemove", function(e){
-            year = this.value;
-            update(year);
-        });
-
-        var runAnimation = false;
-
-        var animationButton = d3.select("button.animation");
-
-        function toggleAnimation() {
-            if (runAnimation === true) {
-                runAnimation = false;
-                animationButton.text("Start");
-            }
-            else if (runAnimation === false) {
-                runAnimation = true;
-                animationButton.text("Stop");
-                animation();
-            }
-        }
-
-        function toggleGraph() {
-            if (svgLine.classed("hidden")) {
-                svgLine.classed("hidden", false);
-            }
-            else {
-                svgLine.classed("hidden", true);
-            }
-        }
-        d3.select("button.graph")
-            .on("click", toggleGraph);
-
-
-        //Add click and drag behaviour for red circle in graph
-        function mouseToYear(mouseCoordinate) {
-            year = Math.round(svgLine.x.invert(mouseCoordinate));
-            if (year < 1900) {
-                update(1900);
-            } else if (year > 2015) {
-                update(2015);
-            } else {
-                update(year);
-            }
-        }
-        var drag = d3.behavior.drag()
-            .on("drag", function(d,i) {
-                mouseToYear(d3.event.x);
-            });
-        svgLine
-            .on("click", function(d,i) {
-                mouseToYear(d3.mouse(svgLine.node())[0]);
-            });
-        svgLine.select("circle").call(drag);
-
-        animationButton
-            .on("click", toggleAnimation);
-
-
-        function update(year) {
-            setYear(year);
-            updateLine(svgLine, year);
-            repaint(gCircles, year, activeStations[year]);
-        }
-
-        function animation() {
-            if (runAnimation === true) {
-                if (year <= 2015) {
-                    update(year);
-                    year++;
-                    setTimeout(animation, 300);
-                } else {
-                    runAnimation = false;
-                    animationButton.text("Start");
-                }
-            }
-        }
-
-        function mouseIn(d,i) {
-             //offsets for tooltips
-            var offsetL = document.getElementById("map").offsetLeft+15;
-            var offsetT = document.getElementById("map").offsetTop+10;
-
-            var mouse = d3.mouse(
-                svg.node()).map( function(d) { return parseInt(d); }
-            );
-            var left = mouse[0]+offsetL;
-            var top = mouse[1]+offsetT;
-            tooltip
-                .classed("hidden", false)
-                .attr(
-                    "style",
-                    "left:" + left + "px;top:" + top +"px"
-                )
-                .html(d.name);
-            d3.select(this).attr("r", circleRadius*4 + "px");
-        }
-
-        function mouseOut(d,i) {
-            tooltip.classed("hidden", true);
-            d3.select(this).attr("r", circleRadius + "px");
-        }
-
-        function getId(d) { return d.id; }
-        function getLong(d) { return d.coordinates[0]; }
-        function getLat(d) { return d.coordinates[1]; }
-
-        function repaint(gCircles, year, currentlyActiveStations) {
-            var circles = gCircles.selectAll("circle")
-                .data(currentlyActiveStations, getId);
-
-            circles
-                .exit()
-                .remove();
-
-            var newCircles = circles
-                .enter()
-                .append("circle");
-
-            newCircles
-                .attr("cx", getLong)
-                .attr("cy", getLat)
-                .attr("r", circleRadius + "px");
-
-            newCircles
-                .on("mousemove", mouseIn)
-                .on("mouseout",  mouseOut);
-        }
+        var stationsStarted = nestByProperty("start_year", weatherStations);
+        var stationsEnded = nestByProperty("end_year", weatherStations);
+        //stations with the field END:2015 are currently running
+        stationsEnded[2015] = [];
+        var nStations = getNumberOfStations(stationsStarted, stationsEnded);
+        var linePlot = new LinePlot(width, height, nStations);
+        stationCircles.activeStations = getActiveStations(stationsStarted, stationsEnded);
+        Interaction(stationCircles, linePlot);
     }
 }
